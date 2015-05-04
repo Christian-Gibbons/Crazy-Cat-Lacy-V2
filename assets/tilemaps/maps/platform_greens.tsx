24c1619d4dabd5e99a217ad1e80cab8bdd5550dc
Christian-Gibbons <cgibbons@gmu.edu>
2015-05-04 16:47:48 -0400

    implement gamestates to original game

--- assets/tilemaps/maps/platform_greens.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tiles_22" tilewidth="16" tileheight="16">
- <image source="../../../../tilesets/cartoony_platformer/tiles_22.png" width="736" height="160"/>
- <terraintypes>
-  <terrain name="brown thing" tile="199"/>
-  <terrain name="grass top" tile="154"/>
-  <terrain name="empty" tile="22"/>
- </terraintypes>
-</tileset>
--- assets/tilemaps/maps/spin_coin_big_strip6.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="spin_coin_big_strip6" tilewidth="9" tileheight="10">
- <image source="../../../../sprites/coin/spin_coin_big_strip6.png" width="54" height="10"/>
- <tile id="0">
-  <properties>
-   <property name="type" value="coin"/>
-  </properties>
- </tile>
-</tileset>
